5dc9983d77597432635be97ee18effcd7d5ff600
hongkailiu <hongkailiu@users.noreply.github.com>
2016-04-23 15:02:16 -0400

    start testng

--- src/test/java/com/example/devtoolindex/helper/HelperTest.java
+++ src/test/java/com/example/devtoolindex/helper/HelperTest.java
@@ -1,8 +1,8 @@
 package com.example.devtoolindex.helper;
 
 import org.apache.commons.lang3.StringUtils;
-import org.junit.Assert;
-import org.junit.Test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import java.net.UnknownHostException;
 
