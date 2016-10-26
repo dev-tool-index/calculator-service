569bec70a5ed22641fc3578feef1cf86a9972ca9
hongkailiu <hongkailiu@users.noreply.github.com>
2016-10-25 22:06:03 -0400

    fix java melody IT

--- src/test/java/com/example/devtoolindex/javamelody/JavaMelodyIT.java
+++ src/test/java/com/example/devtoolindex/javamelody/JavaMelodyIT.java
@@ -1,12 +1,17 @@
 package com.example.devtoolindex.javamelody;
 
 import com.example.devtoolindex.App;
+
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.assertj.core.api.Assertions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
@@ -28,11 +33,46 @@ public class JavaMelodyIT extends
     private RestTemplate template = new TestRestTemplate();
 
     @Test
-    public void testMonitoring() throws Exception {
+    public void testMonitoringWithoutAuth() throws Exception {
         ResponseEntity<String> response = template.getForEntity(BASE_URL_MONITORING + "?format=json", String.class);
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+    }
+
+    @Test
+    public void testMonitoringWithAuth() throws Exception {
+
+        String plainCreds = "admin:dev.tool.index";
+        byte[] plainCredsBytes = plainCreds.getBytes();
+        byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
+        String base64Creds = new String(base64CredsBytes);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + base64Creds);
+
+
+        HttpEntity<String> request = new HttpEntity<>(headers);
+        ResponseEntity<String> response = template.exchange(BASE_URL_MONITORING + "?format=json", HttpMethod.GET, request, String.class);
         Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 
         String body = response.getBody();
         Assertions.assertThat(body).contains("application", "{", "}");
+    }
+
+    @Test
+    public void testMonitoringWithWrongPassword() throws Exception {
+
+        String plainCreds = "admin:password";
+        byte[] plainCredsBytes = plainCreds.getBytes();
+        byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
+        String base64Creds = new String(base64CredsBytes);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + base64Creds);
+
+
+        HttpEntity<String> request = new HttpEntity<>(headers);
+        ResponseEntity<String> response = template.exchange(BASE_URL_MONITORING + "?format=json", HttpMethod.GET, request, String.class);
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+
     }
 }
